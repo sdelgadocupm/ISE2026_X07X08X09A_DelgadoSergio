ce78464447e5ff5e650a0dc21ccf4e5078bab66d
sdelgadocupm <sdelgadoc@alumnos.upm.es>
2026-03-04 17:12:38 +0100

    proyecto rtc con rtos apartado1 inicial

---
+++ RTC/Thread.c
@@ -0,0 +1,123 @@
+#include "cmsis_os2.h"                          // CMSIS RTOS header file
+#include "stm32f4xx_hal.h"
+#include "Thread.h"
+#include "lcd.h"
+
+/*----------------------------------------------------------------------------
+ *      Thread 1 'Thread_Name': Sample thread
+ *---------------------------------------------------------------------------*/
+ 
+osThreadId_t tid_Thread;                        // thread id
+ 
+RTC_HandleTypeDef RtcHandle;
+
+/* Buffers used for displaying Time and Date */
+uint8_t aShowTime[50] = {0};
+uint8_t aShowDate[50] = {0};
+
+static void RTC_CalendarConfig(void);
+static void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate);
+ 
+void Thread (void *argument);                   // thread function
+ 
+int Init_Thread (void) {
+ 
+  tid_Thread = osThreadNew(Thread, NULL, NULL);
+  if (tid_Thread == NULL) {
+    return(-1);
+  }
+ 
+  return(0);
+}
+ 
+void Thread (void *argument) {
+	
+	LCD_reset();
+	LCD_Init();
+	LCD_clean();
+	
+	
+	/*##-1- Configure the RTC peripheral #######################################*/
+  /* Configure RTC prescaler and RTC data registers */
+  /* RTC configured as follows:
+      - Hour Format    = Format 24
+      - Asynch Prediv  = Value according to source clock
+      - Synch Prediv   = Value according to source clock
+      - OutPut         = Output Disable
+      - OutPutPolarity = High Polarity
+      - OutPutType     = Open Drain */ 
+  RtcHandle.Instance = RTC; 
+  RtcHandle.Init.HourFormat = RTC_HOURFORMAT_24;
+  RtcHandle.Init.AsynchPrediv = RTC_ASYNCH_PREDIV;
+  RtcHandle.Init.SynchPrediv = RTC_SYNCH_PREDIV;
+  RtcHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
+  RtcHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  RtcHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  __HAL_RTC_RESET_HANDLE_STATE(&RtcHandle);
+
+	/*##-2- Check if Data stored in BackUp register1: No Need to reconfigure RTC#*/
+  /* Read the Back Up Register 1 Data */
+  if (HAL_RTCEx_BKUPRead(&RtcHandle, RTC_BKP_DR1) != 0x32F2)
+  {
+    /* Configure RTC Calendar */
+    RTC_CalendarConfig();
+  }
+  else
+  {
+    /* Clear source Reset Flag */
+    __HAL_RCC_CLEAR_RESET_FLAGS();
+  }
+		
+  while (1) {
+
+		/*##-3- Display the updated Time and Date ################################*/
+    RTC_CalendarShow(aShowTime, aShowDate);
+	//	printLCD(aShowTime, 1);
+	//	printLCD(aShowDate, 2);
+    osThreadYield();                            // suspend thread
+  }
+}
+
+static void RTC_CalendarConfig(void)
+{
+  RTC_DateTypeDef sdatestructure;
+  RTC_TimeTypeDef stimestructure;
+
+  /*##-1- Configure the Date #################################################*/
+  /* Set Date: Tuesday February 18th 2014 */
+  sdatestructure.Year = 0x14;
+  sdatestructure.Month = RTC_MONTH_FEBRUARY;
+  sdatestructure.Date = 0x18;
+  sdatestructure.WeekDay = RTC_WEEKDAY_TUESDAY;
+
+
+  /*##-2- Configure the Time #################################################*/
+  /* Set Time: 02:00:00 */
+  stimestructure.Hours = 0x02;
+  stimestructure.Minutes = 0x00;
+  stimestructure.Seconds = 0x00;
+  stimestructure.TimeFormat = RTC_HOURFORMAT12_AM;
+  stimestructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE ;
+  stimestructure.StoreOperation = RTC_STOREOPERATION_RESET;
+
+  
+
+  /*##-3- Writes a data in a RTC Backup data Register1 #######################*/
+  HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
+}
+
+static void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate)
+{
+  RTC_DateTypeDef sdatestructureget;
+  RTC_TimeTypeDef stimestructureget;
+
+  /* Get the RTC current Time */
+  HAL_RTC_GetTime(&RtcHandle, &stimestructureget, RTC_FORMAT_BIN);
+  /* Get the RTC current Date */
+  HAL_RTC_GetDate(&RtcHandle, &sdatestructureget, RTC_FORMAT_BIN);
+  /* Display time Format : hh:mm:ss */
+  sprintf((char *)showtime, "%2d:%2d:%2d", stimestructureget.Hours, stimestructureget.Minutes, stimestructureget.Seconds);
+  /* Display date Format : mm-dd-yy */
+  sprintf((char *)showdate, "%2d-%2d-%2d", sdatestructureget.Month, sdatestructureget.Date, 2000 + sdatestructureget.Year);
+}
+
