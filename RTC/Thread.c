9da1450d0a1d2c2a85155be0461a43bd0e14e7bc
sdelgadocupm <sdelgadoc@alumnos.upm.es>
2026-03-06 20:06:01 +0100

    apartado2, biblioteca rtc

--- RTC/Thread.c
+++ RTC/Thread.c
@@ -2,6 +2,7 @@
 #include "stm32f4xx_hal.h"
 #include "Thread.h"
 #include "lcd.h"
+#include "rtc.h"
 
 
 /*----------------------------------------------------------------------------
@@ -26,8 +27,6 @@ RTC_AlarmTypeDef sAlarm;
 uint8_t aShowTime[50] = {0};
 uint8_t aShowDate[50] = {0};
 
-static void RTC_CalendarConfig(void);
-static void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate);
 static void initLEDs(void);
  
 void Thread (void *argument);                   // thread function
@@ -54,53 +53,9 @@ void Thread (void *argument) {
 	
 	initLEDs();
 	
-	 /*##-1- Configure the RTC peripheral #######################################*/
-  /* Configure RTC prescaler and RTC data registers */
-  /* RTC configured as follows:
-      - Hour Format    = Format 24
-      - Asynch Prediv  = Value according to source clock
-      - Synch Prediv   = Value according to source clock
-      - OutPut         = Output Disable
-      - OutPutPolarity = High Polarity
-      - OutPutType     = Open Drain */ 
-  RtcHandle.Instance = RTC; 
-  RtcHandle.Init.HourFormat = RTC_HOURFORMAT_24;
-  RtcHandle.Init.AsynchPrediv = RTC_ASYNCH_PREDIV;
-  RtcHandle.Init.SynchPrediv = RTC_SYNCH_PREDIV;
-  RtcHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
-  RtcHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-  RtcHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-  __HAL_RTC_RESET_HANDLE_STATE(&RtcHandle);
-  if (HAL_RTC_Init(&RtcHandle) != HAL_OK)
-  {
-    /* Initialization Error */
-    //Error_Handler();
-  }
- 
+	configureRTC(&RtcHandle);
+	checkBackUp(&RtcHandle);
 
-  /*##-2- Check if Data stored in BackUp register1: No Need to reconfigure RTC#*/
-  /* Read the Back Up Register 1 Data */
-  if (HAL_RTCEx_BKUPRead(&RtcHandle, RTC_BKP_DR1) != 0x32F2)
-  {
-    /* Configure RTC Calendar */
-    RTC_CalendarConfig();
-  }
-  else
-  {
-    /* Check if the Power On Reset flag is set */
-    if (__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST) != RESET)
-    {
-      
-    }
-    /* Check if Pin Reset flag is set */
-    if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST) != RESET)
-    {
-     
-    }
-    /* Clear source Reset Flag */
-    __HAL_RCC_CLEAR_RESET_FLAGS();
-  }
-	
 	HAL_NVIC_EnableIRQ(RTC_Alarm_IRQn);
 	
 	sAlarm.Alarm=RTC_ALARM_A;
@@ -112,7 +67,7 @@ void Thread (void *argument) {
   while (1) {
 
 		/*##-3- Display the updated Time and Date ################################*/
-    RTC_CalendarShow(aShowTime, aShowDate);
+    RTC_CalendarShow(&RtcHandle, aShowTime, aShowDate);
 		printLCD(aShowTime, 1);
 		printLCD(aShowDate, 2);
 		if(alarma==1){
@@ -148,70 +103,6 @@ static void Timer2_Callback (void const *arg) {
 	}
 }
 
-
-/**
-  * @brief  Configure the current time and date.
-  * @param  None
-  * @retval None
-  */
-static void RTC_CalendarConfig(void)
-{
-  RTC_DateTypeDef sdatestructure;
-  RTC_TimeTypeDef stimestructure;
-
-  /*##-1- Configure the Date #################################################*/
-  /* Set Date: Tuesday February 18th 2014 */
-  sdatestructure.Year = 0x14;
-  sdatestructure.Month = RTC_MONTH_FEBRUARY;
-  sdatestructure.Date = 0x18;
-  sdatestructure.WeekDay = RTC_WEEKDAY_TUESDAY;
-  
-  if(HAL_RTC_SetDate(&RtcHandle,&sdatestructure,RTC_FORMAT_BCD) != HAL_OK)
-  {
-    /* Initialization Error */
-    //Error_Handler();
-  }
-
-  /*##-2- Configure the Time #################################################*/
-  /* Set Time: 02:00:00 */
-  stimestructure.Hours = 0x02;
-  stimestructure.Minutes = 0x00;
-  stimestructure.Seconds = 0x00;
-  stimestructure.TimeFormat = RTC_HOURFORMAT12_AM;
-  stimestructure.DayLightSaving = RTC_DAYLIGHTSAVING_NONE ;
-  stimestructure.StoreOperation = RTC_STOREOPERATION_RESET;
-
-  if (HAL_RTC_SetTime(&RtcHandle, &stimestructure, RTC_FORMAT_BCD) != HAL_OK)
-  {
-    /* Initialization Error */
-    //Error_Handler();
-  }
-
-  /*##-3- Writes a data in a RTC Backup data Register1 #######################*/
-  HAL_RTCEx_BKUPWrite(&RtcHandle, RTC_BKP_DR1, 0x32F2);
-}
-
-
-/**
-  * @brief  Display the current time and date.
-  * @param  showtime : pointer to buffer
-  * @param  showdate : pointer to buffer
-  * @retval None
-  */
-static void RTC_CalendarShow(uint8_t *showtime, uint8_t *showdate)
-{
-  RTC_DateTypeDef sdatestructureget;
-  RTC_TimeTypeDef stimestructureget;
-
-  /* Get the RTC current Time */
-  HAL_RTC_GetTime(&RtcHandle, &stimestructureget, RTC_FORMAT_BIN);
-  /* Get the RTC current Date */
-  HAL_RTC_GetDate(&RtcHandle, &sdatestructureget, RTC_FORMAT_BIN);
-  /* Display time Format : hh:mm:ss */
-  sprintf((char *)showtime, "%02d:%02d:%02d", stimestructureget.Hours, stimestructureget.Minutes, stimestructureget.Seconds);
-  /* Display date Format : mm-dd-yy */
-  sprintf((char *)showdate, "%02d-%02d-%02d", sdatestructureget.Month, sdatestructureget.Date, 2000 + sdatestructureget.Year);
-}
 
 static void initLEDs(void){
   
--- RTC/Thread.h
+++ RTC/Thread.h
@@ -1,8 +1,5 @@
 #ifndef __THREAD_H
 #define __THREAD_H
 	int Init_Thread (void);  
-	
-	/* Defines related to Clock configuration */
-#define RTC_ASYNCH_PREDIV  0x7F   /* LSE as RTC clock */
-#define RTC_SYNCH_PREDIV   0x00FF /* LSE as RTC clock */
+
 #endif
